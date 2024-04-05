c6ad2e72ab1dd9a953abec9cca731f7cb0a2343c
WeiTing Chen <>
2024-04-05 10:49:36 +0200

    update cmake and vcpkg setting with simple gui code

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # cmake project template
+
+```bash
+git submodule https://github.com/microsoft/vcpkg.git
+```
+git clone --recurse-submodules https://github.com/miredirex/cmake-vcpkg-example.git
---
+++ build-setup-osx.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if [ -d "build" ]; then
+    rm -rf build
+    rm -rf ./cache
+fi
+
+mkdir -p build
+
+cmake  -B build -S . -DCMAKE_TOOLCHAIN_FILE=./VCPKG/scripts/buildsystems/vcpkg.cmake
+
+cd build
+mkdir bin
+
+make  -j16
